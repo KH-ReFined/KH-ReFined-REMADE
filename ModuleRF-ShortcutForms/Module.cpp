a738578c635b7b813fe28fd4038f14784d48b631
Topaz Whitelock <topaztk@yahoo.com>
2025-11-29 02:42:19 +0100

    ShortcutForms New Standard

--- ModuleRF-ShortcutForms/Module.cpp
+++ ModuleRF-ShortcutForms/Module.cpp
@@ -1,7 +1,64 @@
-#include "SigScan.h"
 #include <vector>
+#include <Windows.h>
+#include <Psapi.h>
 
 using namespace std;
+
+vector<uint8_t> _instShortcut
+{
+	0xEB, 0x19,
+	0x3C, 0x0B, 0x75, 0x02, 0xB0,
+	0x88, 0x47, 0x01, 0xEB, 0xDC,
+	0xCE,
+	0xEB, 0xAA,
+	0xEB, 0x4E, 0x90, 0x90,
+	0x81, 0xCB, 0x00, 0x00, 0x24, 0x00,
+	0x80, 0xF9, 0x15, 0x74, 0xF2,
+	0xEB, 0x1B, 0x90, 0x90, 0x90, 0x90, 0x90,
+	0x31, 0xC0, 0x48, 0x83, 0xC4, 0x28, 0xC3,
+	0xEB, 0x45, 0x90, 0x90,
+	0x81, 0xC3, 0x00, 0x00, 0x20, 0x00, 0xEB, 0xB5,
+	0x90, 0x90
+};
+
+struct ModuleInfo
+{
+	const char* startAddr;
+	const char* endAddr;
+
+	ModuleInfo()
+	{
+		HMODULE hModule = GetModuleHandle(NULL);
+		startAddr = reinterpret_cast<const char*>(hModule);
+
+		MODULEINFO info = {};
+		GetModuleInformation(GetCurrentProcess(), hModule, &info, sizeof(info));
+		endAddr = startAddr + info.SizeOfImage;
+	}
+};
+
+static const ModuleInfo moduleInfo;
+
+template <typename T>
+T SignatureScan(const char* pattern, const char* mask)
+{
+	size_t patLen = std::strlen(mask);
+
+	for (const char* addr = moduleInfo.startAddr; addr < moduleInfo.endAddr - patLen; ++addr)
+	{
+		size_t i = 0;
+		for (; i < patLen; ++i)
+		{
+			if (mask[i] != '?' && pattern[i] != addr[i])
+				break;
+		}
+
+		if (i == patLen)
+			return reinterpret_cast<T>(const_cast<char*>(addr));
+	}
+
+	return nullptr;
+}
 
 extern "C"
 {
@@ -12,24 +69,11 @@ extern "C"
 		char* _iconOffset = SignatureScan<char*>("\x48\x89\x5C\x24\x08\x57\x48\x83\xEC\x20\x48\x8B\xFA\x41\x0F\xB6\xD9\x41\x0F\xB6\xD0\xE8\x00\x00\x00\x00", "xxxxxxxxxxxxxxxxxxxxxx????");
 		char* _categoryOffset = SignatureScan<char*>("\x48\x89\x5C\x24\x10\x48\x89\x6C\x24\x18\x48\x89\x74\x24\x20\x57\x41\x54\x41\x55\x41\x56\x41\x57\x48\x81\xEC\x90\x01\x00\x00\x48\x8B\x05\x00\x00\x00\x00\x48\x33\xC4\x48\x89\x84\x24\x80\x01\x00\x00\x33\xF6\x89\x4C\x24\x28\x85\xC9\x48\x8D\x05\x00\x00\x00\x00", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx????xxxxxxxxxxxxxxxxxxxxxx????");
 
-		vector<uint8_t> _instShortcut
-		{
-			0xEB, 0x19,
-			0x3C, 0x0B, 0x75, 0x02, 0xB0,
-			0x88, 0x47, 0x01, 0xEB, 0xDC,
-			0xCE,
-			0xEB, 0xAA,
-			0xEB, 0x4E, 0x90, 0x90,
-			0x81, 0xCB, 0x00, 0x00, 0x24, 0x00,
-			0x80, 0xF9, 0x15, 0x74, 0xF2,
-			0xEB, 0x1B, 0x90, 0x90, 0x90, 0x90, 0x90,
-			0x31, 0xC0, 0x48, 0x83, 0xC4, 0x28, 0xC3,
-			0xEB, 0x45, 0x90, 0x90,
-			0x81, 0xC3, 0x00, 0x00, 0x20, 0x00, 0xEB, 0xB5,
-			0x90, 0x90
-		};
+		char* _allocMemory = (char*)malloc(0x19);
 
-		memcpy(_iconOffset + 0x1C, _iconOffset + 0x1D, 0x19);
+		memcpy(_allocMemory, _iconOffset + 0x1D, 0x19);
+
+		memcpy(_iconOffset + 0x1C, _allocMemory, 0x19);
 		memcpy(_iconOffset + 0x1A, _instShortcut.data(), 0x02);
 
 		memcpy(_iconOffset + 0x35, _instShortcut.data() + 0x02, 0x05);
@@ -48,7 +92,5 @@ extern "C"
 		memcpy(_equipOffset + 0x38, _instShortcut.data() + 0x25, 0x07);
 
 		memcpy(_categoryOffset + 0x4AF, _instShortcut.data() + 0x38, 0x02);
-
-		_instShortcut.clear();
 	}
 }
