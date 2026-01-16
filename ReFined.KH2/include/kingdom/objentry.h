578fd4f200bf2da96bc81dedd25a1f6dd1c04094
Topaz Whitelock <topaztk@yahoo.com>
2026-01-16 20:16:43 +0100

    total reorganization.

--- ReFined.KH2/include/kingdom/objentry.h
+++ ReFined.KH2/include/kingdom/objentry.h
@@ -19,6 +19,15 @@ extern "C"
 
 			using ReadRequestWeapon_t = char* (*)(int part, int hand, uint16_t id, int pri, int bank);
 			static ReadRequestWeapon_t ReadRequestWeapon;
+
+			using get_mdlx_t = char* (*)(char* objentryEntry, char* buff);
+			static get_mdlx_t get_mdlx;
+
+			using get_apdx_t = char* (*)(char* objentryEntry, char* buff);
+			static get_apdx_t get_apdx;
+
+			using get_mset_t = char* (*)(char* objentryEntry, uint32_t objectID, char* buff);
+			static get_mset_t get_mset;
 		};
 	}
 }
--- ReFined.KH2/include/kingdom/title.h
+++ ReFined.KH2/include/kingdom/title.h
@@ -14,6 +14,7 @@ extern "C"
 			{
 			public:
 				static bool* IsTitle;
+				static char* Title2LD;
 				static uint8_t* IntroSelect;
 			};
 		}
